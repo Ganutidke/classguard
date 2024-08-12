b3776235346e48d03ddb1b6d963bcce3aef24a49
Seasites <seasites10@gmail.com>
2024-08-12 20:13:56 +0530

    fixed bug - mobile css

--- src/components/UploadSection.js
+++ src/components/UploadSection.js
@@ -1,11 +1,11 @@
+
 'use client'
 import { useState } from 'react';
-import { toast } from 'react-toastify';
 import * as XLSX from 'xlsx';
 
 const UploadSection = () => {
   const [file, setFile] = useState(null);
-  const [minPercentage, setMinPercentage] = useState(75);
+  const [minPercentage, setMinPercentage] = useState(75); // Default value
   const [defaulters, setDefaulters] = useState([]);
   const [downloadVisible, setDownloadVisible] = useState(false);
 
@@ -60,53 +60,52 @@ const UploadSection = () => {
   };
 
   return (
-    <section id="upload" className="py-20 bg-gradient-to-r from-white to-pink-50  lg:bg-hero-upload lg:bg-no-repeat bg-cover">
+    <section id="upload" className="py-20 bg-gradient-to-r from-white to-pink-50 lg:bg-hero-upload lg:bg-no-repeat lg:bg-cover">
       <div className="container mx-auto text-center">
         <h2 className="text-3xl font-bold mb-8">Upload Excel Sheet</h2>
-        
-         
-
         <div className="bg-white shadow-lg rounded-lg p-8 mb-8 max-w-xl mx-auto">
           {/* File Upload Section */}
-         <div className="flex items-center justify-center w-full mb-8">
-          <label 
-            htmlFor="dropzone-file" 
-            className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
-          >
-            <div className="flex flex-col items-center justify-center pt-5 pb-6">
-              <svg 
-                className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" 
-                aria-hidden="true" 
-                xmlns="http://www.w3.org/2000/svg" 
-                fill="none" 
-                viewBox="0 0 20 16"
-              >
-                <path 
-                  stroke="currentColor" 
-                  strokeLinecap="round" 
-                  strokeLinejoin="round" 
-                  strokeWidth="2" 
-                  d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                />
-              </svg>
-              <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                <span className="font-semibold">Click to upload</span> or drag and drop
-              </p>
-              <p className="text-xs text-gray-500 dark:text-gray-400">
-                Excel Sheet Only
-              </p>
-            </div>
-            <input 
-              id="dropzone-file" 
-              type="file" 
-              className="hidden" 
-              onChange={handleFileChange} 
-            />
-          </label>
-        </div>
+          <div className="flex items-center justify-center w-full mb-8">
+            <label
+              htmlFor="dropzone-file"
+              className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+            >
+              <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                <svg
+                  className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 16"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                  />
+                </svg>
+                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                  <span className="font-semibold">Click to upload</span> or drag and drop
+                </p>
+                <p className="text-xs text-gray-500 dark:text-gray-400">
+                  Excel Sheet Only
+                </p>
+              </div>
+              <input
+                id="dropzone-file"
+                type="file"
+                className="hidden"
+                onChange={handleFileChange}
+              />
+            </label>
+          </div>
+          
+          {/* Minimum Attendance Percentage Input */}
           <div className="mb-4">
-            <label 
-              htmlFor="percentage" 
+            <label
+              htmlFor="percentage"
               className="block text-left text-gray-700 text-xs lg:text-lg lg:font-medium"
             >
               Minimum Attendance Percentage:
@@ -119,6 +118,8 @@ const UploadSection = () => {
               className="mt-2 p-2 w-full border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
             />
           </div>
+          
+          {/* Process File Button */}
           <button
             onClick={processFile}
             className="w-full bg-gray-900 text-white py-2 px-4 rounded hover:bg-gray-700 transition"
@@ -130,34 +131,35 @@ const UploadSection = () => {
           </p>
         </div>
 
-
         {/* Result Section */}
         <div id="result" className="mt-8 max-w-xl mx-auto">
           {defaulters.length > 0 ? (
             <>
               <h3 className="text-xl font-bold mb-4">Defaulter List:</h3>
-              <table className="table-auto w-full text-left">
-                <thead>
-                  <tr>
-                    <th className="px-4 py-2">Roll Number</th>
-                    <th className="px-4 py-2">Name</th>
-                    <th className="px-4 py-2">Total Lectures</th>
-                    <th className="px-4 py-2">Lectures Attended</th>
-                    <th className="px-4 py-2">Percentage</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {defaulters.map((student, index) => (
-                    <tr key={index}>
-                      <td className="border px-4 py-2">{student['Roll Number']}</td>
-                      <td className="border px-4 py-2">{student.Name}</td>
-                      <td className="border px-4 py-2">{student['Total Lectures']}</td>
-                      <td className="border px-4 py-2">{student['Lectures Attended']}</td>
-                      <td className="border px-4 py-2">{student.Percentage}%</td>
+              <div className="overflow-x-auto">
+                <table className="table-auto w-full text-left">
+                  <thead>
+                    <tr>
+                      <th className="px-4 py-2">Roll Number</th>
+                      <th className="px-4 py-2">Name</th>
+                      <th className="px-4 py-2">Total Lectures</th>
+                      <th className="px-4 py-2">Lectures Attended</th>
+                      <th className="px-4 py-2">Percentage</th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
+                  </thead>
+                  <tbody>
+                    {defaulters.map((student, index) => (
+                      <tr key={index} className="border-b">
+                        <td className="border px-4 py-2">{student['Roll Number']}</td>
+                        <td className="border px-4 py-2">{student.Name}</td>
+                        <td className="border px-4 py-2">{student['Total Lectures']}</td>
+                        <td className="border px-4 py-2">{student['Lectures Attended']}</td>
+                        <td className="border px-4 py-2">{student.Percentage}%</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
               {downloadVisible && (
                 <button
                   onClick={downloadFile}
@@ -177,3 +179,5 @@ const UploadSection = () => {
 };
 
 export default UploadSection;
+
+
